204796f3821a67caba2fdcad80a60f0e0e0142f6
Vinicius Mansur <vinim1403@gmail.com>
2026-04-07 22:38:50 -0300

    Feat: Novo arquivo Script2.js adicionado

---
+++ Script2.js
@@ -0,0 +1,5 @@
+alert("Bem vindo a esse site!");
+let nome = prompt("Digite seu nome");
+let idade = prompt("Digite sua idade");
+let prof = prompt("Digite sua profissão");
+alert(`Olá ${nome}, você tem ${idade} anos e trabalha como ${prof}`)
